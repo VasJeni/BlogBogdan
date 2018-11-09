9af422d59a54d2351444d8e01bc000ec567aaee3
JasJeni <Vasilyev508@gmail.com>
2018-11-09 12:13:06 +0200

    add major changes

--- app/js/common.js
+++ app/js/common.js
@@ -1,35 +1,55 @@
 $(function() {
+    $(document).ready(function(){
+        $('.nav-link').click(function(){
+            $('.nav-link #nav-icon1').toggleClass('open');
+            $('.dropdown-menu').toggleClass('show');
+        });
+    });
 
-	// Custom JS
+    $(document).ready(function(){
+        $(".lectures__images").owlCarousel({
+            items: 1,
+            loop : true,
+            nav : true,
+            dots : true,
+            margin: 20
+        });
+        $(".sp__carousel").owlCarousel({
+            items: 1,
+            loop : true,
+            nav : true,
+            dots : true,
+            margin : 20
+        });
+        $(".sp__carouse2").owlCarousel({
+            items: 1,
+            loop : true,
+            nav : true,
+            dots : true,
+            margin : 20
+        });
+        $(".school__carousel").owlCarousel({
+            items: 1,
+            loop : true,
+            nav : true,
+            dots : true,
+            margin : 20
+        });
+        $(".home__carousel").owlCarousel({
+            items: 1,
+            loop : true,
+            nav : false,
+            dots : true,
+            margin : 0
+        });
+    });
 
-});
-$(document).ready(function(){
-    $(".lectures__images").owlCarousel({
-        items: 1,
-        loop : true,
-        nav : true,
-        dots : true,
-        margin: 20
+    $(document).click(function(e) {
+        if (!$(e.target).is('.dropdown-menu, .nav-link, #nav-icon1, #nav-icon1 span, .dropdown-item')) {
+            $('#nav-icon1').removeClass('open');
+            $('.dropdown-menu').removeClass('show');
+        }
     });
-    $(".sp__carousel").owlCarousel({
-        items: 1,
-        loop : true,
-        nav : true,
-        dots : true,
-        margin : 20
-    });
-    $(".sp__carouse2").owlCarousel({
-        items: 1,
-        loop : true,
-        nav : true,
-        dots : true,
-        margin : 20
-    });
-    $(".school__carousel").owlCarousel({
-        items: 1,
-        loop : true,
-        nav : true,
-        dots : true,
-        margin : 20
-    });
+
+
 });
